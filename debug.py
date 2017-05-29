dcc9bac845b4a830675c4eaa4d1c4265620f9f32
Gregg Parrish <gregg.alb@gmail.com>
2017-05-29 15:32:29 +0200

    fix bandcamp link formatter for embeds

--- debug.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-
-link = 'https://bandcamp.com/EmbeddedPlayer/album=25834495/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/'
-player = BeautifulSoup(requests.get(link).content, 'lxml')
-link = player.find('input', { "id" : "shareurl" } ).get('value') 
-print(link)
